9c551b16b57c15c69667ce3524011f038fac3a8d
eleve <elevenfive1@gmail.com>
2023-04-06 23:43:41 +0600

    Connected fragments through the event bus EventBus

--- app/src/main/java/com/example/lab_20/BlankFragment1.kt
+++ app/src/main/java/com/example/lab_20/BlankFragment1.kt
@@ -5,55 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment1.newInstance] factory method to
- * create an instance of this fragment.
- */
+class FragmentChangeEvent(val fragmentClass: Class<out Fragment>)
 class BlankFragment1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var btn_fragment1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        EventBus.getDefault().register(this)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank1, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_fragment1 = view.findViewById(R.id.btn_fragment1)
+        btn_fragment1.setOnClickListener {
+            EventBus.getDefault().post(FragmentChangeEvent(BlankFragment2::class.java))
+        }
+    }
+
+    @Subscribe (threadMode = ThreadMode.MAIN)
+    fun onFragmentChange(event: FragmentChangeEvent) {
+        val fragmentClass = event.fragmentClass
+        val fragment = fragmentClass.newInstance()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EventBus.getDefault().unregister(this)
     }
 }
--- app/src/main/java/com/example/lab_20/MainFragment.kt
+++ app/src/main/java/com/example/lab_20/MainFragment.kt
@@ -7,13 +7,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import com.example.lab_20.FragmentChangeEvent
 
 interface OnButtonClickListener {
     fun onButtonClicked()
 }
+
 class MainFragment : Fragment() {
     private lateinit var button: Button
     private var onButtonClickListener: OnButtonClickListener? = null
+    lateinit var btn_fragment1: Button
+    lateinit var btn_fragment2: Button
+    lateinit var btn_fragment3: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        EventBus.getDefault().register(this)
+    }
+
+    @Subscribe (threadMode = ThreadMode.MAIN)
+    fun onFragmentChange(event: FragmentChangeEvent) {
+        val fragmentClass = event.fragmentClass
+        val fragment = fragmentClass.newInstance()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -25,14 +48,6 @@ class MainFragment : Fragment() {
         onButtonClickListener = null
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        button = view.findViewById(R.id.button)
-        button.setOnClickListener {
-            onButtonClickListener?.onButtonClicked()
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -41,4 +56,25 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button = view.findViewById(R.id.button)
+        button.setOnClickListener {
+            onButtonClickListener?.onButtonClicked()
+        }
+        btn_fragment1 = view.findViewById(R.id.btn_fragment1)
+        btn_fragment2 = view.findViewById(R.id.btn_fragment2)
+        btn_fragment3 = view.findViewById(R.id.btn_fragment3)
+        btn_fragment1.setOnClickListener {
+            EventBus.getDefault().post(FragmentChangeEvent(BlankFragment1::class.java))
+        }
+        btn_fragment2.setOnClickListener {
+            EventBus.getDefault().post(FragmentChangeEvent(BlankFragment2::class.java))
+        }
+        btn_fragment3.setOnClickListener {
+            EventBus.getDefault().post(FragmentChangeEvent(BlankFragment3::class.java))
+        }
+
+
+    }
 }
